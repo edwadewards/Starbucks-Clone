3df3f3bd6f820ec76affba4470d8dd2a970ad522
Wade Edwards <wadecedwards@gmail.com>
2022-01-31 23:06:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function openTabs(evt, tabName) {
+  const tabContent = document.getElementsByClassName('tab-content');
+  for (let i = 0; i < tabContent.length; i++) {
+  tabContent[i].style.display = 'none';
+  }
+
+  const tabLinks = document.querySelectorAll('.tablinks');
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = 'flex';
+  evt.currentTarget.className += ' active';
+}
+
+
+
+const hamburger = document.querySelector('.nav__hamburger--btn');
+const menu = document.querySelector('.nav__menu--mobile');
+const closeMenu = document.querySelector('.close-btn');
+
+hamburger.addEventListener('click', () => {
+  menu.style.transform = 'translateX(0%)';
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.style.transform = 'translateX(100%)';
+})
